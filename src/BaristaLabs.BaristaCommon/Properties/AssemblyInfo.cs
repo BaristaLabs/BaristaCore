c17147e3a334cd38ec0644754235f86c237fe8d2
Sean McLellan <sean.mclellan@baristalabs.com>
2016-11-16 09:20:02 -0500

    Stub out a BaristaCommon project that will contain interfaces/common types

---
+++ src/BaristaLabs.BaristaCommon/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BaristaLabs.BaristaCommon")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16a6d8b0-b004-426c-9c09-dc2e7d9df2a6")]
