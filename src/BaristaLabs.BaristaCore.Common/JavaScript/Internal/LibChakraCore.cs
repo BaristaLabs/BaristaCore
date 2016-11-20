15496f52f93e6a5225ee277fda154350dc70dfff
Oceanswave <sean.mclellan@sixconcepts.com>
2016-11-19 23:40:18 -0500

    TravisCI: Testing...

--- src/BaristaLabs.BaristaCore.Common/JavaScript/Internal/LibChakraCore.cs
+++ src/BaristaLabs.BaristaCore.Common/JavaScript/Internal/LibChakraCore.cs
@@ -8,7 +8,7 @@
 	///</summary>
 	public static class LibChakraCore
 	{
-		private const string DllName = "ChakraCore/libChakraCore";
+		private const string DllName = "ChakraCore/libChakraCore.dylib";
 
 		/// <summary>
 		///     Initialize a ModuleRecord from host
