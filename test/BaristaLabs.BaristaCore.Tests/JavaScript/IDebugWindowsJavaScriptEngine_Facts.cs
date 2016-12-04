24dd7cc909dbf66111cbc7693276f30fd2d3e262
Sean McLellan <sean@baristalabs.io>
2016-12-04 10:15:58 -0500

    UnitTests: Add test for windows-only JsDiagEvaluate

---
+++ test/BaristaLabs.BaristaCore.Tests/JavaScript/IDebugWindowsJavaScriptEngine_Facts.cs
@@ -0,0 +1,102 @@
+﻿namespace BaristaLabs.BaristaCore.JavaScript.Tests
+{
+    using System;
+    using System.Text;
+    using Xunit;
+
+    public class IDebugWindowsJavaScriptEngine_Facts
+    {
+        private IJavaScriptEngine Engine;
+
+        public IDebugWindowsJavaScriptEngine_Facts()
+        {
+            Engine = JavaScriptEngineFactory.CreateChakraEngine();
+        }
+
+        public ICommonWindowsJavaScriptEngine CommonWindowsEngine
+        {
+            get
+            {
+                return Engine as ICommonWindowsJavaScriptEngine;
+            }
+        }
+
+        public IDebugWindowsJavaScriptEngine DebugWindowsEngine
+        {
+            get
+            {
+                return Engine as IDebugWindowsJavaScriptEngine;
+            }
+        }
+
+        /// <summary>
+        /// End-to-end test of debugging
+        /// </summary>
+        [Fact]
+        public void JsDiagEvaluateReturnsAValue()
+        {
+            if (DebugWindowsEngine == null)
+                return;
+
+            string iterate = @"
+var moose = 0;
+for(var i = 0; i < 50; i++)
+{
+    moose++;
+}
+
+moose;
+";
+            int iPod = 0;
+
+            using (var runtimeHandle = Engine.JsCreateRuntime(JavaScriptRuntimeAttributes.None, null))
+            {
+                using (var contextHandle = Engine.JsCreateContext(runtimeHandle))
+                {
+                    Engine.JsSetCurrentContext(contextHandle);
+
+                    //Callback that is run for each breakpoint.
+                    JavaScriptDiagDebugEventCallback callback = (JavaScriptDiagDebugEventType eventType, IntPtr eventData, IntPtr callbackState) =>
+                    {
+                        //JsDiagEvaluate differs from JsDiagEvaluateUtf8 in that it returns an object.
+                        var evalResultHandle = DebugWindowsEngine.JsDiagEvaluate("i", 0);
+                        
+                        var handleType = Engine.JsGetValueType(evalResultHandle);
+                        Assert.Equal(JavaScriptValueType.Object, handleType);
+
+                        iPod++;
+
+                        return true;
+                    };
+
+                    using (var ss = new ScriptSource(Engine, iterate))
+                    {
+                        Engine.JsDiagStartDebugging(runtimeHandle, callback, IntPtr.Zero);
+
+                        var scripts = Engine.JsDiagGetScripts();
+                        Assert.NotEqual(JavaScriptValueSafeHandle.Invalid, scripts);
+
+                        var ix = Engine.JsIntToNumber(0);
+                        var objScriptHandle = Engine.JsGetIndexedProperty(scripts, ix);
+                        var scriptIdPropertyHandle = CommonWindowsEngine.JsGetPropertyIdFromName("scriptId");
+                        var scriptIdHandle = Engine.JsGetProperty(objScriptHandle, scriptIdPropertyHandle);
+
+                        var scriptId = Engine.JsNumberToInt(scriptIdHandle);
+
+                        //Set a breakpoint with a knkown position
+                        var breakPointHandle = Engine.JsDiagSetBreakpoint((uint)scriptId, 4, 0);
+                        Assert.NotEqual(JavaScriptValueSafeHandle.Invalid, breakPointHandle);
+
+                        var finalResult = Engine.JsCallFunction(ss.FunctionHandle, new IntPtr[] { ss.FunctionHandle.DangerousGetHandle() }, 1);
+                        var handleType = Engine.JsGetValueType(finalResult);
+                        Assert.Equal(JavaScriptValueType.Number, handleType);
+                        
+                        Engine.JsDiagStopDebugging(runtimeHandle);
+                    }
+
+                    Assert.Equal(50, iPod);
+                }
+            }
+        }
+    }
+}
---
+++ test/BaristaLabs.BaristaCore.Tests/JavaScript/ScriptSource.cs
@@ -0,0 +1,91 @@
+﻿namespace BaristaLabs.BaristaCore.JavaScript.Tests
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Represents a 'scriptsource' implementation.
+    /// </summary>
+    public sealed class ScriptSource : IDisposable
+    {
+        private IntPtr m_ptrScript;
+        private readonly string m_script;
+        private readonly JavaScriptSourceContext m_sourceContext;
+
+        private JavaScriptValueSafeHandle m_scriptHandle, m_sourceUrlHandle, m_fnScript;
+
+        public string Script
+        {
+            get { return m_script; }
+        }
+
+        /// <summary>
+        /// Gets the handle of the script
+        /// </summary>
+        public JavaScriptValueSafeHandle FunctionHandle
+        {
+            get { return m_fnScript; }
+        }
+
+        public JavaScriptSourceContext SourceContext
+        {
+            get { return m_sourceContext; }
+        }
+
+        public ScriptSource(IJavaScriptEngine engine, string script, string sourceUrl = "[eval source]")
+        {
+            if (engine == null)
+                throw new ArgumentNullException(nameof(engine));
+
+            if (script == null)
+                throw new ArgumentNullException(nameof(script));
+
+            m_script = script;
+            m_ptrScript = Marshal.StringToHGlobalAnsi(script);
+            m_scriptHandle = engine.JsCreateExternalArrayBuffer(m_ptrScript, (uint)script.Length, null, IntPtr.Zero);
+            m_sourceUrlHandle = engine.JsCreateStringUtf8(sourceUrl, new UIntPtr((uint)sourceUrl.Length));
+            m_sourceContext = JavaScriptSourceContext.GetNextSourceContext();
+
+            m_fnScript = engine.JsParse(m_scriptHandle, m_sourceContext, m_sourceUrlHandle, JavaScriptParseScriptAttributes.None);
+        }
+
+        #region IDisposable
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (m_fnScript != null)
+                {
+                    m_fnScript.Dispose();
+                    m_fnScript = null;
+                }
+
+                if (m_sourceUrlHandle != null)
+                {
+                    m_sourceUrlHandle.Dispose();
+                    m_sourceUrlHandle = null;
+                }
+
+                if (m_scriptHandle != null)
+                {
+                    m_scriptHandle.Dispose();
+                    m_scriptHandle = null;
+                }
+            }
+
+            Marshal.FreeHGlobal(m_ptrScript);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~ScriptSource()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
