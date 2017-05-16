8ce2519e3bf027c152a38a5feb2ca8b702d43926
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-05-16 18:04:52 -0400

    Initial Changes

---
+++ util/ChakraWrapperGeneratorCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ChakraWrapperGeneratorCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
