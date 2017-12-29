4073f5bc4b8be9ec0d3df1d6ef1ed878c457177d
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-12-29 07:09:01 -0500

    Shim up typescript

--- src/BaristaLabs.BaristaCore.Extensions/Modules/TypeScriptModule.cs
+++ src/BaristaLabs.BaristaCore.Extensions/Modules/TypeScriptModule.cs
@@ -3,11 +3,11 @@
     using System.Threading.Tasks;
 
     [BaristaModule("typescript", "TypeScript is a language for application scale JavaScript development", Version = "2.6.2")]
-    public class TypeScriptModule : IBaristaScriptModule
+    public class TypeScriptModule : INodeModule
     {
         public Task<object> ExportDefault(BaristaContext context, BaristaModuleRecord referencingModule)
         {
-            return Task.FromResult<object>(Properties.Resources.react_production_min);
+            return Task.FromResult<object>(Properties.Resources.typescript_min);
         }
     }
 }
---
+++ test/BaristaLabs.BaristaCore.Extensions.Tests/TypeScript_Facts.cs
@@ -0,0 +1,52 @@
+﻿namespace BaristaLabs.BaristaCore.Extensions.Tests
+{
+    using BaristaLabs.BaristaCore.Extensions;
+    using BaristaLabs.BaristaCore.ModuleLoaders;
+    using BaristaLabs.BaristaCore.Modules;
+    using Microsoft.Extensions.DependencyInjection;
+    using Newtonsoft.Json;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Xunit;
+
+    [ExcludeFromCodeCoverage]
+    public class TypeScript_Facts
+    {
+        public IBaristaRuntimeFactory GetRuntimeFactory()
+        {
+            var myMemoryModuleLoader = new InMemoryModuleLoader();
+            myMemoryModuleLoader.RegisterModule(new TypeScriptModule());
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBaristaCore(moduleLoader: myMemoryModuleLoader);
+
+            var provider = serviceCollection.BuildServiceProvider();
+            return provider.GetRequiredService<IBaristaRuntimeFactory>();
+        }
+
+        [Fact]
+        public void CanUseReactComponent()
+        {
+            var script = @"
+import ts from 'typescript';
+
+
+export default 'foo';
+        ";
+
+            var baristaRuntime = GetRuntimeFactory();
+
+            using (var rt = baristaRuntime.CreateRuntime())
+            {
+                using (var ctx = rt.CreateContext())
+                {
+                    using (ctx.Scope())
+                    {
+                        var response = ctx.EvaluateModule(script);
+                        Assert.NotNull(response);
+                    }
+                }
+            }
+        }
+    }
+}
