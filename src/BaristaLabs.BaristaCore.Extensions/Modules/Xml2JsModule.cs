39000f9c4f052876f5a679e6846186243d724361
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-12-29 13:09:09 -0500

    Stub out Xml2Js module

---
+++ src/BaristaLabs.BaristaCore.Extensions/Modules/Xml2JsModule.cs
@@ -0,0 +1,16 @@
+﻿namespace BaristaLabs.BaristaCore.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [BaristaModule("xml2js", "Simple XML to JavaScript object converter.")]
+    public class Xml2JsModule : IBaristaModule
+    {
+        public Task<object> ExportDefault(BaristaContext context, BaristaModuleRecord referencingModule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
